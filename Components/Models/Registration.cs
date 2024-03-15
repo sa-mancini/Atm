5e184d0f6ccec0758b499b044c936e9100fe0742
sa-mancini <sara.mancini01@gmail.com>
2024-03-15 12:02:45 +0100

    frontend1

--- Components/Models/Registration.cs
+++ Components/Models/Registration.cs
@@ -1,10 +1,14 @@
 using @System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using FormApp.Components.Pages;
 
 namespace FormApp.Models;
 
 public class Registration 
 {
+    [Required(ErrorMessage = "Categoria cliente obbligatoria")]
+    public string CategoriaCliente { get; set; }
+
     [Required(ErrorMessage ="Cognome obbligatorio")]
     [StringLength(20, ErrorMessage = "Cognome troppo lungo. Massimo 20 caratteri.")]
     public string? Cognome { get; set;}
@@ -13,43 +17,65 @@ public class Registration
     [StringLength(20, ErrorMessage = "Nome troppo lungo. Massimo 20 caratteri.")]
     public string? Nome { get; set; }
 
+    [Required(ErrorMessage = "Sesso obbligatorio")]
+    public string? Sesso { get; set; }
+
     [Required(ErrorMessage = "Luogo di nascita obbligatorio")]
     public string? LuogoDiNascita { get; set; }
 
     [Required(ErrorMessage = "Data di nascita obbligatoria")]
-    public DateOnly? DataDiNascita { get; set; }
+    public DateOnly DataDiNascita { get; set; }
 
     [Required(ErrorMessage = "Via di residenza obbligatoria")]
-    public string? ViaResidenza { get; set; }
+    public string ViaResidenza { get; set; }
 
     [Required(ErrorMessage = "Numero civico di residenza obbligatorio")]
-    public int? NCivResidenza { get; set; }
+    public int NCivResidenza { get; set; }
 
     [Required(ErrorMessage = "CAP di residenza obbligatorio")]
-    public int? CapResidenza { get; set; }
+    public int CapResidenza { get; set; }
 
     [Required(ErrorMessage = "Comune di residenza obbligatorio")]
-    public string? ComuneResidenza { get; set; }
+    public string ComuneResidenza { get; set; }
 
     [Required(ErrorMessage = "Provincia di residenza obbligatoria")]
-    public string? ProvinciaResidenza { get; set; }
+    public string ProvinciaResidenza { get; set; }
 
-    public string? ViaDomicilio { get; set; }
+    public string ViaDomicilio { get; set; }
 
-    public int? NCivDomicilio { get; set; }
+    public int NCivDomicilio { get; set; }
 
-    public int? CapDomicilio { get; set; }
+    public int CapDomicilio { get; set; }
 
-    public string? ComuneDomicilio { get; set; }
+    public string ComuneDomicilio { get; set; }
 
-    public string? ProvinciaDomicilio { get; set; }
+    public string ProvinciaDomicilio { get; set; }
 
     [Required(ErrorMessage = "Cittadinanza obbligatoria")]
-    public int? Cittadinanza { get; set; }
+    public string Cittadinanza { get; set; }
 
     [Required(ErrorMessage = "Codice fiscale obbligatorio")]
-    public string? CodiceFiscale { get; set; }
+    public string CodiceFiscale { get; set; }
 
-    [Required(ErrorMessage = "Email obbligatoria")]
-    public string? Email { get; set; }
+    public string Email { get; set; }
+
+    public int Telefono { get; set; }
+
+    public int TelefonoUfficio { get; set; }
+
+    [Required(ErrorMessage = "Codice Abbonamento obbligatorio")]
+    public int CodiceAbbonamento { get; set; }
+
+    [Required(ErrorMessage = "Codice Matricola obbligatorio")]
+    public int MatricolaDipendente { get; set; }
+
+    [Required(ErrorMessage = "Stato obbligatorio")]
+    public string Stato { get; set; }
+
+    [Required(ErrorMessage = "Tipo obbligatorio")]
+    public string Tipo1 { get; set; }
+    
+    public string Tipo2 { get; set; }
+
 }
+
--- obj/Debug/net8.0/FormApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FormApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3161fe9acf7c709b2d55e3b30d997174d70b44bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
